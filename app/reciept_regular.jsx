946a9e53a8db951505415fbd3bd223bff2dbffd1
AmarHasecic <ahasecic1@etf.unsa.ba>
2024-07-10 14:33:45 +0200

    Some UI fix

--- app/reciept_regular.jsx
+++ app/reciept_regular.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image } from 'react-native';
 import LottieView from 'lottie-react-native';
 import { useLocalSearchParams } from 'expo-router'
 import { Dimensions, PixelRatio } from 'react-native';
@@ -15,10 +15,11 @@ const scaleFont = (size) => {
 const RecieptRegular = () => {
 
   const  params  = useLocalSearchParams();
+  const date = Date();
 
   return (
     <View style={
-      styles.row
+      styles.column
     }>
       <View style={styles.moneyBar}>
 
@@ -37,7 +38,33 @@ const RecieptRegular = () => {
           loop
         />
       </View>
-      
+      <View style={styles.containerInfo}>
+
+      <View style={ styles.row }>
+            <Image 
+                      style={styles.icon}
+                      source={require('../assets/images/calendar.png')}
+            />
+            <Text style={styles.textInfo}>{date}</Text>
+          </View>
+
+          <View style={ styles.row }>
+            <Image 
+                      style={styles.icon}
+                      source={require('../assets/images/playlogo.png')}
+            />
+            <Text style={styles.textInfo}>{params.consoleNum}</Text>
+          </View>
+
+          <View style={ styles.row }>
+            <Image 
+                      style={styles.icon}
+                      source={require('../assets/images/clock.png')}
+            />
+            <Text style={styles.textInfo}>{params.timeSpent}</Text>
+          </View>
+
+        </View>
     </View>
   );
 }
@@ -53,11 +80,15 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     position: 'absolute',
-    top: "-45%", 
+    top: "-45%"
+  },
+  column: {
+    flex: 1,
+    flexDirection: 'column',
   },
   row: {
     flex: 1,
-    flexDirection: 'column'
+    flexDirection: 'row'
   },
   moneyBar: {
     backgroundColor: "#ff4500",
@@ -77,6 +108,27 @@ const styles = StyleSheet.create({
     adjustsFontSizeToFit: true,
     allowFontScaling: true
   },
+  icon: {
+      flex: 1,
+      width: '100%', 
+      height: '100%',
+      resizeMode: "contain" 
+    },
+  containerInfo: {
+      flexDirection: 'column',
+      width: "50%",
+      height: "65%",
+      position: 'absolute',
+      paddingTop: "70%",
+      marginTop: "7%",
+      gap: 15
+  },
+  textInfo: {
+    flex: 1,
+    width: "100%",
+    height: "100%"
+  }
+  
 });
 
 export default RecieptRegular;
