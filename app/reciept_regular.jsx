154796f6527b48643b5a6cd21f4add285f89369f
AmarHasecic <ahasecic1@etf.unsa.ba>
2024-07-11 23:26:35 +0200

    Animation removed

--- app/reciept_regular.jsx
+++ app/reciept_regular.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { View, StyleSheet, Text, Image } from 'react-native';
+import { View, StyleSheet, Text, Image, TouchableOpacity, Dimensions, PixelRatio } from 'react-native';
 import LottieView from 'lottie-react-native';
-import { useLocalSearchParams } from 'expo-router'
-import { Dimensions, PixelRatio } from 'react-native';
- 
+import { useLocalSearchParams, useNavigation } from 'expo-router';
 
 const scaleFont = (size) => {
   const screenWidth = Dimensions.get('window').width;
@@ -14,136 +12,149 @@ const scaleFont = (size) => {
 
 const RecieptRegular = () => {
 
-  const  params  = useLocalSearchParams();
+  const params = useLocalSearchParams();
   const date = Date();
+  const navigation = useNavigation();
 
   return (
-    <View style={
-      styles.column
-    }>
-    <View style={{flex: 2}}>
-      <View style={styles.moneyBar}>
+      <View style={styles.column}>
+        <View style={styles.moneyBar}>
+          <View style={styles.time}>
+            <Text style={styles.timeText}>
+              {params.moneySpent} KM
+            </Text>
+          </View>
+        </View>
+        <View style={styles.containerInfo}>
+          <View style={styles.row}>
+            <Image 
+              style={styles.icon}
+              source={require('../assets/images/calendar.png')}
+            />
+            <View style={styles.textInfo}>
+              <Text style={styles.text}>{date}</Text>
+            </View>
+          </View>
 
-        <View style={styles.time}>
-          <Text style={styles.timeText}>
-            {params.moneySpent} KM
-          </Text>
+          <View style={styles.row}>
+            <Image 
+              style={styles.iconLogo}
+              source={require('../assets/images/playlogo.png')}
+            />
+            <View style={styles.textInfo}>
+              <Text style={styles.text}>{params.consoleNum}</Text>
+            </View>
+          </View>
+
+          <View style={styles.row}>
+            <Image 
+              style={styles.icon}
+              source={require('../assets/images/clock.png')}
+            />
+            <View style={styles.textInfo}>
+              <Text style={styles.text}>{params.timeSpent}</Text>
+            </View>
+          </View>
         </View>
 
-      </View>
-      <View style={styles.container}>
-        <LottieView
-          source={require("../assets/animations/waves.json")}
-          style={styles.animation}
-          autoPlay
-          loop
-        />
-      </View>
-      <View style={styles.containerInfo}>
+        <View style={styles.blank} />
 
-      <View style={ styles.row }>
-            <Image 
-                      style={styles.icon}
-                      source={require('../assets/images/calendar.png')}
-            />
-            <View style={styles.textInfo}>
-              <Text>{date}</Text>
-            </View>
-          </View>
-
-          <View style={ styles.row }>
-            <Image 
-                      style={styles.icon}
-                      source={require('../assets/images/playlogo.png')}
-            />
-            <View style={styles.textInfo}>
-              <Text>{params.consoleNum}</Text>
-            </View>
-          </View>
-
-          <View style={ styles.row }>
-            <Image 
-                      style={styles.icon}
-                      source={require('../assets/images/clock.png')}
-            />
-            <View style={styles.textInfo}>
-              <Text>{params.timeSpent}</Text>
-            </View>
-          </View>
-
+        <View style={styles.buttonBlock}>
+          <TouchableOpacity style={styles.button} onPress={() => navigation.popToTop()}>
+            <Text style={styles.buttonText}>Završi</Text>
+          </TouchableOpacity> 
         </View>
-
-       <View style={styles.blank}>
-
-       </View>
-    </View>
-    </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    justifyContent: 'flex-start', 
-    alignItems: 'center',
-  },   
-  animation: {
-    width: "100%",
-    height: "100%",
-    position: 'absolute',
-    top: "-45%",
-
-  },
   column: {
     flex: 1,
     flexDirection: 'column',
   },
-  row: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
   moneyBar: {
     backgroundColor: "#ff4500",
-    width: "100%",
-    height: "35%"
+    height: scaleFont(170), 
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   time: {
-    flex: 1
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   timeText: {
-    flex: 1,
     textAlign: 'center',
-    fontSize: scaleFont(50),
-    paddingTop: "25%",
+    fontSize: 50,
     color: 'rgba(0, 0, 0, 0.7)',
     fontWeight: 'bold',
     adjustsFontSizeToFit: true,
-    allowFontScaling: true
+    allowFontScaling: true,
+  },
+  animation: {
+    width: scaleFont(500),
+    height: scaleFont(500),
+    position: 'absolute',
+  },
+  containerInfo: {
+    flex: 2,
+    flexDirection: 'column',
+    width: scaleFont(300),
+    gap: 10,
+    paddingTop: 30
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   icon: {
-      flex:1,
-      width: '100%', 
-      height: '100%',
-      resizeMode: "contain" 
-    },
-  containerInfo: {
-      flex: 2,
-      flexDirection: 'column',
-      width: "70%",
-      gap: 10
+    flex: 1,
+    width: '100%', 
+    height: '100%',
+    resizeMode: "contain",
+  },
+  iconLogo: {
+    flex: 1,
+    width: '87%', 
+    height: '87%',
+    resizeMode: "contain",
   },
   textInfo: {
     flex: 2,
-    width: "100%",
-    height: "100%",
-    fontSize: scaleFont(13),
     justifyContent: 'center',
   },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 17,
+    color: 'rgba(0, 0, 0, 0.7)',
+  },
   blank: {
-    flex: 3
-  } 
+    flex: 3,
+  },
+  buttonBlock: {
+    position: 'absolute', 
+    bottom: 20, 
+    left: 0,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: "3%",
+    paddingHorizontal: "35%",
+    borderRadius: 9,
+    elevation: 2,
+    backgroundColor: '#ff4500',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 13,
+    color: 'white',
+  }
 });
 
 export default RecieptRegular;
